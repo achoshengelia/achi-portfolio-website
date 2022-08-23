3a2521b636284b8407fd87786e38fa896bf81737
Achi Shengelia <archil.shengelia@altexsoft.com>
2022-08-23 21:49:43 +0400

    add Video component

--- src/components/project-details/Video/Video.jsx
+++ src/components/project-details/Video/Video.jsx
@@ -1,27 +1,111 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import video from 'assets/test.mp4';
-import { ContainerStyled, VideoStyled } from './VideoStyles';
+import {
+  CircularTextStyled,
+  ContainerStyled,
+  CursorWrapperStyled,
+  VideoStyled
+} from './VideoStyles';
 
 const Video = () => {
   const videoRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
   const togglePlay = () => {
     const video = videoRef.current;
-
     if (video.paused) {
       video.play();
+      setIsPlaying(true);
     } else {
       video.pause();
+      setIsPlaying(false);
     }
   };
 
   return (
     <ContainerStyled>
-      <VideoStyled ref={videoRef}>
+      <VideoStyled
+        data-cursor
+        ref={videoRef}
+        onClick={togglePlay}
+        isPlaying={isPlaying}
+      >
         <source src={video} />
       </VideoStyled>
-      <button onClick={togglePlay}>Play</button>
+      <Cursor isPlaying={isPlaying} />
     </ContainerStyled>
   );
 };
 
 export default Video;
+
+const Cursor = ({ isPlaying }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const cursorRef = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener('mousemove', e => {
+      cursorRef.current.style.top = `${e.clientY - 70}px`;
+      cursorRef.current.style.left = `${e.clientX - 70}px`;
+    });
+
+    document.addEventListener('mouseover', e => {
+      if (e.target.dataset.cursor) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    });
+
+    return () => {
+      document.removeEventListener('mousemove', e => {
+        cursorRef.current.style.inset = `${e.clientY - 15}px auto auto ${
+          e.clientX - 15
+        }px`;
+      });
+    };
+  }, [cursorRef]);
+
+  return (
+    <CursorWrapperStyled ref={cursorRef} isVisible={isVisible && !isPlaying}>
+      <CircularText />
+    </CursorWrapperStyled>
+  );
+};
+
+const CircularText = () => {
+  return (
+    <CircularTextStyled id="container">
+      <svg
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        x="0px"
+        y="0px"
+        viewBox="0 0 150 150"
+        enableBackground="new 0 0 150 150"
+        xmlSpace="preserve"
+      >
+        <defs>
+          <path
+            id="circlePath"
+            d="
+        M 75, 75
+        m -75, 0
+        a 75,75 0 1,0 150,0
+        a 75,75 0 1,0 -150,0
+        "
+          />
+        </defs>
+        <circle cx="75" cy="75" r="75" fill="none" />
+        <g>
+          <use xlinkHref="#circlePath" fill="none" />
+          <text>
+            <textPath xlinkHref="#circlePath" textLength={460}>
+              Play Video • Play Video • Play Video • Play Video •
+            </textPath>
+          </text>
+        </g>
+      </svg>
+    </CircularTextStyled>
+  );
+};
--- src/components/project-details/Video/VideoStyles.js
+++ src/components/project-details/Video/VideoStyles.js
@@ -1,8 +1,53 @@
 import styled from 'styled-components';
+import play from 'assets/icons/play.svg';
+import pause from 'assets/icons/pause.svg';
+
+export const CircularTextStyled = styled.div`
+  position: relative;
+  width: 15rem;
+  height: 15rem;
+  overflow: hidden;
+  animation: rotate 5s infinite linear;
+
+  & text {
+    font-size: 2rem;
+    fill: ${props => props.theme.colors.text.accent};
+  }
+
+  @keyframes rotate {
+    from {
+      transform: rotate(360deg);
+    }
+    to {
+      transform: rotate(0);
+    }
+  }
+`;
+
+export const CursorWrapperStyled = styled.i`
+  height: 10rem;
+  width: 10rem;
+  border-radius: 100%;
+  position: fixed;
+  z-index: 99000;
+  pointer-events: none;
+  color: red;
+  transform: ${({ isVisible }) => (isVisible ? 'scale(1)' : 'scale(0)')};
+  transition: transform 0.1s ease-in-out;
+  box-shadow: ${({ isPointer }) =>
+    isPointer ? '0 0 30px rgba(255, 255, 0, 0.5)' : null};
+  font-style: normal;
+`;
 
 export const VideoStyled = styled.video`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
+  max-height: 90vh;
+  opacity: ${({ isPlaying }) => (isPlaying ? '1' : '.3')};
+  transition: opacity 0.7s ease;
+
+  cursor: ${({ isPlaying }) =>
+    isPlaying ? `url(${pause}) 6 8, auto` : `url(${play}) 6 8, auto`};
 `;
 
 export const ContainerStyled = styled.section`
@@ -10,5 +55,8 @@ export const ContainerStyled = styled.section`
   background-color: ${props => props.theme.colors.text.main};
   border-radius: 2rem 2rem 0 0;
   padding: 5rem 3rem;
-  margin-top: 8rem;
+  padding-top: 8rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
