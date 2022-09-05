a5dac4e3d52c5c36920258dcf0c6c0e0301571ed
Achi Shengelia <archil.shengelia@altexsoft.com>
2022-09-05 15:53:16 +0400

    add Hero responsive

--- src/components/about/Hero/HeroStyles.js
+++ src/components/about/Hero/HeroStyles.js
@@ -29,4 +29,27 @@ export const ContainerStyled = styled.section`
       margin-top: 5rem;
     }
   }
+
+  @media ${props => props.theme.breakpoints.lg} {
+    font-size: 5rem;
+  }
+
+  @media ${props => props.theme.breakpoints.md} {
+    ${ParagraphStyled} {
+      max-width: none;
+    }
+  }
+
+  @media ${props => props.theme.breakpoints.sm} {
+    font-size: 4rem;
+
+    ${ParagraphStyled} {
+      & > div {
+        max-width: 4rem;
+        max-height: 4rem;
+        display: inline-block;
+        transform: translateY(25%);
+      }
+    }
+  }
 `;
