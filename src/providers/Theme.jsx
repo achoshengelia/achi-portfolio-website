6ab2db50618c60f35be081c12c767db8865e0109
Achi Shengelia <archil.shengelia@altexsoft.com>
2022-11-19 16:05:38 +0400

    fix: build error

--- src/providers/Theme.jsx
+++ src/providers/Theme.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import defaultTheme from 'styles/themes/default';
+import defaultTheme from '../styles/themes/default';
 
 export const Theme = ({ children }) => {
   return <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>;
